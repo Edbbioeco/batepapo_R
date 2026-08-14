65f3ece0640bc245a2abd167a48fe160c635a620
Edbbioeco <edsonbbiologia@gmail.com>
2026-08-13 21:17:41 -0300

    Pesquisar catálogo

--- temperatura_caatinga.R
+++ temperatura_caatinga.R
@@ -34,3 +34,14 @@ ggplot() +
 
 cliente <- CDSE::GetOAuthClient(id = Sys.getenv("CDSE_id"),
                                 secret = Sys.getenv("CDSE_secret"))
+
+## Buscar catálogo ----
+
+catalogo <- CDSE::SearchCatalog(aoi = caa,
+                                from = "2000-01-01",
+                                to = "2026-07-01",
+                                collection = "sentinel-3-slstr-l2",
+                                filter = "eo:cloud_cover < 1",
+                                client = cliente)
+
+catalogo
