1aa74f03ac56614bf554bf8eece528ff7e778e8e
Edbbioeco <edsonbbiologia@gmail.com>
2026-08-13 21:36:02 -0300

    Baixar rasters

--- temperatura_caatinga.R
+++ temperatura_caatinga.R
@@ -74,3 +74,28 @@ function evaluatePixel(sample) {
 }"
 
 evalscript
+
+## Baixar rasters ----
+
+dir.create("./temp")
+
+purrr::map(datas,
+           purrr::in_parallel(
+
+             ~CDSE::GetImage(bbox = caa |> sf::st_bbox(),
+                             script = evalscript,
+                             time_range = .x,
+                             file = paste0("./temp/temp_",
+                                           .x,
+                                           ".tif"),
+                             collection = "sentinel-3-slstr-l2",
+                             format = "image/tiff",
+                             mosaicking_order = "leastRecent",
+                             resolution = 1000,
+                             mask = TRUE,
+                             buffer = 100,
+                             client = cliente)
+
+           ),
+           .progress = TRUE)
+
